34a1355622f0a2f5bf5921553847075602e8f9a5
Anders Law <anderstlaw@gmail.com>
2024-08-17 20:39:14 -0400

    moved files around

---
+++ trampled flowers/tfjava.js
@@ -0,0 +1,9 @@
+function showsidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'inline-block'
+}
+
+function hidesidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
